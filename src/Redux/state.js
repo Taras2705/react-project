471c66452f326a5f92401438b3cb0cf8e0a92ee8
Taras <taraskusiy@gmail.com>
2022-06-16 15:14:38 +0300

    create store,refactoring state

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,154 +1,166 @@
-import {rerenderEntireTree} from "../render";
+let store={
+    _state:{
+        newsFeedPage: {
+            posts: [
+                {
+                    id: 1,
+                    postImg: 'https://themified.com/friend-finder/images/post-images/1.jpg',
+                    personIco: 'https://themified.com/friend-finder/images/users/user-5.jpg',
+                    name: 'Alexis Clark',
+                    like: '13',
+                    dislike: '0',
+                    infoStatus: 'Published a photo about 3 mins ago',
+                    message: 'Lorem ipsum dolor sit amet,\n' +
+                        '                      consectetur adipiscing elit, sed do eiusmod tempor\n' +
+                        '                incididunt ut labore et dolore magna aliqua.\n' +
+                        '                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n' +
+                        '                consequat.\n' +
+                        '                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n' +
+                        '                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est\n' +
+                        '                laborum.'
+                },
+                {
+                    id: 2,
+                    postImg: 'https://themified.com/friend-finder/images/post-images/2.jpg',
+                    personIco: 'https://themified.com/friend-finder/images/users/user-3.jpg',
+                    name: 'Sophia Lee',
+                    like: '75',
+                    dislike: '8',
+                    infoStatus: 'Updated her status about 33 mins ago',
+                    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
+                        '                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
+                        '                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+                }
 
-let state = {
-    newsFeedPage: {
-        posts: [
-            {
-                id: 1,
-                postImg: 'https://themified.com/friend-finder/images/post-images/1.jpg',
-                personIco: 'https://themified.com/friend-finder/images/users/user-5.jpg',
-                name: 'Alexis Clark',
-                like: '13',
-                dislike: '0',
-                infoStatus: 'Published a photo about 3 mins ago',
-                message: 'Lorem ipsum dolor sit amet,\n' +
-                    '                      consectetur adipiscing elit, sed do eiusmod tempor\n' +
-                    '                incididunt ut labore et dolore magna aliqua.\n' +
-                    '                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\n' +
-                    '                consequat.\n' +
-                    '                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n' +
-                    '                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est\n' +
-                    '                laborum.'
-            },
-            {
-                id: 2,
-                postImg: 'https://themified.com/friend-finder/images/post-images/2.jpg',
-                personIco: 'https://themified.com/friend-finder/images/users/user-3.jpg',
-                name: 'Sophia Lee',
-                like: '75',
-                dislike: '8',
-                infoStatus: 'Updated her status about 33 mins ago',
-                message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
-                    '                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
-                    '                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-            }
 
+            ],
+            newPostText: '',
+        },
 
-        ],
-        newPostText: '',
-    },
+        messagesPage: {
+            dialogs: [
+                {
+                    id: 1, userImg: 'https://themified.com/friend-finder/images/users/user-2.jpg',
+                    userName: 'Linda Lohan', userMessage: 'Hi there,how are you?'
+                },
+                {
+                    id: 2, userImg: 'https://themified.com/friend-finder/images/users/user-10.jpg',
+                    userName: 'Julia Cox',
+                    userMessage: 'see you soon'
+                },
+                {
+                    id: 3, userImg: 'https://themified.com/friend-finder/images/users/user-3.jpg',
+                    userName: 'Sophia Lee',
+                    userMessage: 'okay fine.thank you'
+                },
+                {
+                    id: 4, userImg: 'https://themified.com/friend-finder/images/users/user-4.jpg',
+                    userName: 'John Doe',
+                    userMessage: 'Hi there,how are you?'
+                },
+                {
+                    id: 5, userImg: 'https://themified.com/friend-finder/images/users/user-9.jpg',
+                    userName: 'Anna Young',
+                    userMessage: 'I gotta go'
+                }
 
-    messagesPage: {
-        dialogs: [
-            {
-                id: 1, userImg: 'https://themified.com/friend-finder/images/users/user-2.jpg',
-                userName: 'Linda Lohan', userMessage: 'Hi there,how are you?'
-            },
-            {
-                id: 2, userImg: 'https://themified.com/friend-finder/images/users/user-10.jpg',
-                userName: 'Julia Cox',
-                userMessage: 'see you soon'
-            },
-            {
-                id: 3, userImg: 'https://themified.com/friend-finder/images/users/user-3.jpg',
-                userName: 'Sophia Lee',
-                userMessage: 'okay fine.thank you'
-            },
-            {
-                id: 4, userImg: 'https://themified.com/friend-finder/images/users/user-4.jpg',
-                userName: 'John Doe',
-                userMessage: 'Hi there,how are you?'
-            },
-            {
-                id: 5, userImg: 'https://themified.com/friend-finder/images/users/user-9.jpg',
-                userName: 'Anna Young',
-                userMessage: 'I gotta go'
-            }
-
-        ],
-        messages: [
-            {
+            ],
+            messages: [
+                {
+                    id: 1, userImg: 'https://themified.com/friend-finder/images/users/user-2.jpg',
+                    userName: 'Linda Lohan',
+                    userMessage: 'Hi honey, how are you doing???? Long time no see. Where have you been?'
+                },
+                {
+                    id: 2, userImg: 'https://themified.com/friend-finder/images/users/user-1.jpg',
+                    userName: 'Sarah Cruiz',
+                    userMessage: 'Hi honey, how are you doing???? Long time no see. Where have you been?'
+                },
+                {
+                    id: 3, userImg: 'https://themified.com/friend-finder/images/users/user-1.jpg',
+                    userName: 'Sarah Cruiz',
+                    userMessage: 'Hi honey, how are you doing???? Long time no see. Where have you been?'
+                }
+            ],
+            newMessageText: ''
+        },
+        sidebar: {
+            users: [{
                 id: 1, userImg: 'https://themified.com/friend-finder/images/users/user-2.jpg',
                 userName: 'Linda Lohan',
-                userMessage: 'Hi honey, how are you doing???? Long time no see. Where have you been?'
             },
-            {
-                id: 2, userImg: 'https://themified.com/friend-finder/images/users/user-1.jpg',
-                userName: 'Sarah Cruiz',
-                userMessage: 'Hi honey, how are you doing???? Long time no see. Where have you been?'
-            },
-            {
-                id: 3, userImg: 'https://themified.com/friend-finder/images/users/user-1.jpg',
-                userName: 'Sarah Cruiz',
-                userMessage: 'Hi honey, how are you doing???? Long time no see. Where have you been?'
-            }
-        ],
-        newMessageText: ''
+                {
+                    id: 2, userImg: 'https://themified.com/friend-finder/images/users/user-10.jpg',
+                    userName: 'Julia Cox',
+
+                },
+                {
+                    id: 3, userImg: 'https://themified.com/friend-finder/images/users/user-3.jpg',
+                    userName: 'Sophia Lee',
+
+                },
+                {
+                    id: 4, userImg: 'https://themified.com/friend-finder/images/users/user-4.jpg',
+                    userName: 'John Doe',
+
+                },
+                {
+                    id: 5, userImg: 'https://themified.com/friend-finder/images/users/user-9.jpg',
+                    userName: 'Anna Young',
+
+                }
+
+            ]
+        }
     },
-    sidebar: {
-        users: [{
-            id: 1, userImg: 'https://themified.com/friend-finder/images/users/user-2.jpg',
-            userName: 'Linda Lohan',
-        },
+    getState(){
+        return(this._state)
+    },
+    _callSubscriber(){
+        console.log('state changed')
+    },
+    addPost ()  {
+        let newPost =
             {
-                id: 2, userImg: 'https://themified.com/friend-finder/images/users/user-10.jpg',
-                userName: 'Julia Cox',
+                id: 3,
+                personIco: 'https://themified.com/friend-finder/images/users/user-1.jpg',
+                name: 'Sarah Cruiz',
+                like: '0',
+                dislike: '0',
+                infoStatus: '',
+                message: this._state.newsFeedPage.newPostText
 
-            },
-            {
-                id: 3, userImg: 'https://themified.com/friend-finder/images/users/user-3.jpg',
-                userName: 'Sophia Lee',
+            };
+        this._state.newsFeedPage.posts.push(newPost);
+        this._state.newsFeedPage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (textPost) {
+        this._state.newsFeedPage.newPostText = textPost;
+        this._callSubscriber(this._state)
+    },
+    sendMessage ()  {
+        let newTextMessage = {
+            id: 4,
+            userImg: 'https://themified.com/friend-finder/images/users/user-1.jpg',
+            userName: 'Sarah Cruiz',
+            userMessage: this._state.messagesPage.newMessageText
 
-            },
-            {
-                id: 4, userImg: 'https://themified.com/friend-finder/images/users/user-4.jpg',
-                userName: 'John Doe',
-
-            },
-            {
-                id: 5, userImg: 'https://themified.com/friend-finder/images/users/user-9.jpg',
-                userName: 'Anna Young',
-
-            }
-
-        ]
-    }
-}
-export let addPost = () => {
-    let newPost =
-        {
-            id: 3,
-            personIco: 'https://themified.com/friend-finder/images/users/user-1.jpg',
-            name: 'Sarah Cruiz',
-            like: '0',
-            dislike: '0',
-            infoStatus: '',
-            message: state.newsFeedPage.newPostText
-
-        };
-    state.newsFeedPage.posts.push(newPost);
-    state.newsFeedPage.newPostText = '';
-    rerenderEntireTree(state);
-}
-export let updateNewPostText = (textPost) => {
-    state.newsFeedPage.newPostText = textPost;
-    rerenderEntireTree(state)
-}
-export let sendMessage = () => {
-    let newTextMessage = {
-        id: 4,
-        userImg: 'https://themified.com/friend-finder/images/users/user-1.jpg',
-        userName: 'Sarah Cruiz',
-        userMessage: state.messagesPage.newMessageText
+        }
+        this._state.messagesPage.messages.push(newTextMessage);
+        this._state.messagesPage.newMessageText = '';
+        this._callSubscriber(this._state)
+    },
+    updateNewDialogsText (textDialogs) {
+        this._state.messagesPage.newMessageText = textDialogs;
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer){
+        this._callSubscriber=observer;
 
     }
-    state.messagesPage.messages.push(newTextMessage);
-    state.messagesPage.newMessageText = '';
-    rerenderEntireTree(state)
+
 }
-export let updateNewDialogsText = (textDialogs) => {
-    state.messagesPage.newMessageText = textDialogs;
-    rerenderEntireTree(state)
-}
-export default state;
+
+export default store;
 
